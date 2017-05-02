684fc968d2c69b0352dc596fff64e955f4ac4344
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2017-05-02 15:04:46 +0800

    dangling changes

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -4,3 +4,4 @@ alias docker-stopped='printf "\n>>> Deleting stopped containers\n\n" && docker r
 alias docker-untagged='printf "\n>>> Deleting untagged images\n\n" && docker rmi $(docker images -q -f dangling=true)'
 alias docker-rmi='printa "\n>>> Deleting all unused images\n\n" && docker rmi $(docker ps -q)'
 alias docker-dangling='docker volume rm $(docker volume ls -qf dangling=true)'
+alias ansible="docker run --rm --interactive --tty -v /etc/timezone:/etc/timezone -v `pwd`:/etc/ansible -v ${HOME}/.ssh:/data/.ssh -it muccg/ansible"
